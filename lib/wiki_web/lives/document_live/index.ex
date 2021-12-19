df5333f10f7fc28914b249326ffa1d6c533bd0f6
ByeongUk Choi <cbw0916@gmail.com>
2021-12-19 11:24:15 +0900

    Add document index test data

--- lib/wiki_web/lives/document_live/index.ex
+++ lib/wiki_web/lives/document_live/index.ex
@@ -3,6 +3,26 @@ defmodule WikiWeb.DocumentLive.Index do
 
   def mount(_params, _session, socket) do
     # temperature = Thermostat.get_user_reading(user_id)
-    {:ok, assign(socket, items: [], page: 1, total_page: 2, per_page: 10)}
+    test_items = [
+      %{
+        id: 1,
+        title: "Hello",
+        author_name: "Tom",
+        created_at: DateTime.utc_now() |> DateTime.truncate(:second)
+      },
+      %{
+        id: 2,
+        title: "World",
+        author_name: "James",
+        created_at: DateTime.utc_now() |> DateTime.truncate(:second)
+      },
+      %{
+        id: 3,
+        title: "Good",
+        author_name: "Mike",
+        created_at: DateTime.utc_now() |> DateTime.truncate(:second)
+      }
+    ]
+    {:ok, assign(socket, items: test_items, page: 1, total_page: 1, per_page: 10)}
   end
 end
