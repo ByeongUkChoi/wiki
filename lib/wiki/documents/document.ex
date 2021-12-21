034284454d78b754e45fd01b29df9ce2e49e8b32
ByeongUk Choi <cbw0916@gmail.com>
2021-12-22 00:06:52 +0900

    Add document new validate

--- lib/wiki/documents/document.ex
+++ lib/wiki/documents/document.ex
@@ -1,10 +1,19 @@
 defmodule Wiki.Documents.Document do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "documents" do
     field :title, :string
     field :content, :string
 
     timestamps()
+  end
+
+  @doc false
+  def changeset(document, attrs) do
+    document
+    |> cast(attrs, [:title, :content])
+    |> validate_required([:title, :content])
+    |> validate_length(:title, max: 255)
   end
 end
--- lib/wiki_web/lives/document_live/new.ex
+++ lib/wiki_web/lives/document_live/new.ex
@@ -1,10 +1,21 @@
 defmodule WikiWeb.DocumentLive.New do
   use WikiWeb, :live_view
 
+  import Ecto.Changeset
+
   alias Wiki.Documents.Document
 
   def mount(_params, _session, socket) do
-    changeset = Ecto.Changeset.change(%Document{})
-    {:ok, assign(socket, changeset: changeset)}
+    {:ok, assign(socket, changeset: change(%Document{}))}
+  end
+
+  def handle_event("validate", %{"document" => document}, socket) do
+    changeset =
+      %Document{}
+      |> Document.changeset(document)
+      |> Map.put(:action, :insert)
+
+    IO.inspect(changeset)
+    {:noreply, assign(socket, changeset: changeset)}
   end
 end
