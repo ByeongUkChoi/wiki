66066ce3fbf2a6982a8edbff43c12755c80b0c6d
ByeongUk Choi <cbw0916@gmail.com>
2021-12-25 12:32:35 +0900

    Fix document index test data

--- lib/wiki_web/lives/document_live/index.ex
+++ lib/wiki_web/lives/document_live/index.ex
@@ -3,27 +3,32 @@ defmodule WikiWeb.DocumentLive.Index do
 
   def mount(_params, _session, socket) do
     # temperature = Thermostat.get_user_reading(user_id)
-    test_items = [
-      %{
-        id: 1,
-        title: "Hello",
-        author_name: "Tom",
-        created_at: DateTime.utc_now() |> DateTime.truncate(:second)
-      },
-      %{
-        id: 2,
-        title: "World",
-        author_name: "James",
-        created_at: DateTime.utc_now() |> DateTime.truncate(:second)
-      },
-      %{
-        id: 3,
-        title: "Good",
-        author_name: "Mike",
-        created_at: DateTime.utc_now() |> DateTime.truncate(:second)
-      }
-    ]
+    items = test_items()
 
-    {:ok, assign(socket, items: test_items, page: 1, total_page: 1, per_page: 10)}
+    {:ok, assign(socket, items: items, page: 1, total_page: 1, per_page: 10)}
+  end
+
+  def handle_event("keydown", _, socket) do
+    {:noreply, socket}
+  end
+
+  defp test_items() do
+    titles = ["Hello", "World", "Good", "Starcraft", "Happy", "Show me the money", "Hell", "TIL"]
+    authors = ["Tom", "James", "Mike", "Kim", "Lee", "Ali", "King"]
+    random_title = fn titles -> Enum.random(titles) end
+    random_author = fn authors -> Enum.random(authors) end
+
+    random_time = fn ->
+      DateTime.utc_now() |> DateTime.add(Enum.random(-1..-9)) |> DateTime.truncate(:second)
+    end
+
+    Enum.map(1..30, fn i ->
+      %{
+        id: i,
+        title: random_title.(titles),
+        author_name: random_author.(authors),
+        created_at: random_time.()
+      }
+    end)
   end
 end
