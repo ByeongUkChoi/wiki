5816606f60b4684ad8d2885787146795488b958b
ByeongUk Choi <cbw0916@gmail.com>
2022-01-02 12:42:44 +0900

    Add DocumentStore.fetch_all/1

--- lib/wiki/document_store/gen_server_impl.ex
+++ lib/wiki/document_store/gen_server_impl.ex
@@ -29,6 +29,14 @@ defmodule Wiki.DocumentStore.GenServerImpl do
   end
 
   @impl true
+  def fetch_all(page_num: page_num, per_page: per_page) do
+    documents = GenServer.call(__MODULE__, {:fetch_all, page_num, per_page})
+
+    documents
+    |> Enum.map(&struct(Document, &1))
+  end
+
+  @impl true
   def create(title: title, content: content) do
     id = next_id()
     GenServer.cast(__MODULE__, {:create, id, %{title: title, content: content}})
@@ -46,8 +54,16 @@ defmodule Wiki.DocumentStore.GenServerImpl do
 
   @impl true
   def handle_call({:fetch_by_id, id}, _from, init_param) do
-    new_state = Map.get(init_param, id)
+    new_state = init_param |> Map.get(id) |> Map.put(:id, id)
     {:reply, new_state, init_param}
+  end
+
+  @impl true
+  def handle_call({:fetch_all, page_num, per_page}, _from, init_param) do
+    start_index = (page_num - 1) * per_page
+    ids = init_param |> Map.keys() |> Enum.slice(start_index, per_page)
+    documents = ids |> Enum.map(&(init_param |> Map.get(&1) |> Map.put(:id, &1)))
+    {:reply, documents, init_param}
   end
 
   @impl true
