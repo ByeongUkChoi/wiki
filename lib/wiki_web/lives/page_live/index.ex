5dd3f4507a32d62381d44d01d52f6c2a860a62aa
ByeongUk Choi <cbw0916@gmail.com>
2022-01-28 23:11:25 +0900

    Add pages index tree

--- lib/wiki_web/lives/page_live/index.ex
+++ lib/wiki_web/lives/page_live/index.ex
@@ -3,15 +3,44 @@ defmodule WikiWeb.PageLive.Index do
 
   def render(assigns) do
     ~H"""
-    <ul>
-      <%= for item <- @pages do %>
-        <li><%= item.name %></li>
-      <% end %>
-    </ul>
+    <aside class="menu">
+      <p class="menu-label">Pages</p>
+      <%= render_pages(%{socket: @socket, pages: @pages}) %>
+    </aside>
     """
   end
 
+  defp render_pages(assigns) do
+    ~H"""
+      <ul class="menu-list">
+        <%= for item <- @pages do %>
+          <li>
+            <a><%= item.name %></a>
+            <%= if has_children?(item.children) do %>
+              <%= render_pages(%{socket: @socket, pages: item.children}) %>
+            <% end %>
+          </li>
+        <% end %>
+      </ul>
+    """
+  end
+
+  defp has_children?([]), do: false
+  defp has_children?(c) when is_list(c), do: true
+  defp has_children?(_), do: false
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, pages: [%{name: "Hello world!"}])}
+    {:ok,
+     assign(socket,
+       pages: [
+         %{
+           name: "Hello world!",
+           children: [
+             %{name: "Chapter 1", children: [%{name: "Chapter 1-1", children: []}]},
+             %{name: "Chapter 2", children: []}
+           ]
+         }
+       ]
+     )}
   end
 end
