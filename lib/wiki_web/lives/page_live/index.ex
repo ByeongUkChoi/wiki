98c9977bc29d24f27f0c0aca0be68d78a91ba1cb
ByeongUk Choi <cbw0916@gmail.com>
2022-02-03 23:47:46 +0900

    fix page index live view

--- lib/wiki_web/lives/page_live/index.ex
+++ lib/wiki_web/lives/page_live/index.ex
@@ -13,34 +13,62 @@ defmodule WikiWeb.PageLive.Index do
   defp render_pages(assigns) do
     ~H"""
       <ul class="menu-list">
-        <%= for item <- @pages do %>
-          <li>
-            <a><%= item.name %></a>
-            <%= if has_children?(item.children) do %>
-              <%= render_pages(%{socket: @socket, pages: item.children}) %>
-            <% end %>
-          </li>
+        <%= for page <- @pages do %>
+          <%= render_page(%{socket: @socket, page: page}) %>
         <% end %>
       </ul>
     """
   end
 
-  defp has_children?([]), do: false
-  defp has_children?(c) when is_list(c), do: true
-  defp has_children?(_), do: false
+  # ▼ ► ●
+  defp render_page(assigns) do
+    ~H"""
+      <li>
+        <%= if @page.has_children do %>
+          <%= if @page.opened do %>
+            <span>▼</span>
+          <% else %>
+            <span>►</span>
+          <% end %>
+        <% else %>
+          <span>●</span>
+        <% end %>
+        <a><%= @page.name %></a>
+        <%= if @page.opened do %>
+          <%= render_pages(%{socket: @socket, pages: @page.children}) %>
+        <% else %>
+      </li>
+    """
+  end
 
   def mount(_params, _session, socket) do
-    {:ok,
-     assign(socket,
-       pages: [
-         %{
-           name: "Hello world!",
-           children: [
-             %{name: "Chapter 1", children: [%{name: "Chapter 1-1", children: []}]},
-             %{name: "Chapter 2", children: []}
-           ]
-         }
-       ]
-     )}
+    {:ok, assign(socket, pages: get_pages(nil))}
+  end
+
+  # test data
+  defp get_pages(parent_id) do
+    pages = all_pages()
+
+    pages
+    |> Enum.filter(&(&1.parent_id == parent_id))
+    |> Enum.map(&Map.put(&1, :children, []))
+    |> Enum.map(&Map.put(&1, :has_children, true))
+    |> Enum.map(&Map.put(&1, :opened, true))
+  end
+
+  defp all_pages do
+    [
+      %{id: 1, name: "Ch 1", parent_id: nil},
+      %{id: 2, name: "Ch 2", parent_id: nil},
+      %{id: 3, name: "Ch 3", parent_id: nil},
+      %{id: 4, name: "Ch 4", parent_id: nil},
+      %{id: 5, name: "Ch 5", parent_id: nil},
+      %{id: 6, name: "Ch 1-1", parent_id: 1},
+      %{id: 7, name: "Ch 1-2", parent_id: 1},
+      %{id: 8, name: "Ch 1-3", parent_id: 1},
+      %{id: 9, name: "Ch 1-2-1", parent_id: 7},
+      %{id: 10, name: "Ch 1-2-2", parent_id: 7},
+      %{id: 11, name: "Ch 3-1", parent_id: 7}
+    ]
   end
 end
