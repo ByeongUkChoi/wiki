fe81591439d56b7d9638393cd17e264f59b6e118
ByeongUk Choi <cbw0916@gmail.com>
2022-02-14 19:39:59 +0900

    Add append children page

--- lib/wiki_web/lives/page_live/index.ex
+++ lib/wiki_web/lives/page_live/index.ex
@@ -28,7 +28,7 @@ defmodule WikiWeb.PageLive.Index do
     ~H"""
       <li>
         <%= live_patch render_index_button(%{socket: @socket, page: @page}),
-          to: Routes.page_path(@socket, :index, [page_id: @page.id]), style: "display: inline" %>
+          to: Routes.page_path(@socket, :index, page_id: @page.id), style: "display: inline" %>
         <%= live_patch @page.name, to: Routes.page_path(@socket, :show, @page.id), style: "display: inline" %>
         <%= render_pages(%{socket: @socket, pages: @page.children}) %>
       </li>
@@ -51,13 +51,22 @@ defmodule WikiWeb.PageLive.Index do
 
   def mount(_params, %{"id" => id} = _session, socket) do
     pages = get_pages(nil)
-
-    # TODO: update childpage
-    # if id != nil do
-    #   children = get_pages(id)
-    # end
+    pages = append_children(pages, id)
 
     {:ok, assign(socket, pages: pages)}
+  end
+
+  defp append_children(pages, nil) do
+    pages
+  end
+
+  defp append_children(pages, id) do
+    Enum.map(pages, fn page ->
+      case page do
+        %{id: ^id} -> Map.put(page, :children, get_pages(id))
+        _ -> page
+      end
+    end)
   end
 
   # test data
@@ -72,17 +81,17 @@ defmodule WikiWeb.PageLive.Index do
 
   defp all_pages do
     [
-      %{id: 1, name: "Ch 1", parent_id: nil, has_children: true, children: []},
-      %{id: 2, name: "Ch 2", parent_id: nil, has_children: false, children: []},
-      %{id: 3, name: "Ch 3", parent_id: nil, has_children: false, children: []},
-      %{id: 4, name: "Ch 4", parent_id: nil, has_children: false, children: []},
-      %{id: 5, name: "Ch 5", parent_id: nil, has_children: false, children: []},
-      %{id: 6, name: "Ch 1-1", parent_id: 1, has_children: false, children: []},
-      %{id: 7, name: "Ch 1-2", parent_id: 1, has_children: true, children: []},
-      %{id: 8, name: "Ch 1-3", parent_id: 1, has_children: false, children: []},
-      %{id: 9, name: "Ch 1-2-1", parent_id: 7, has_children: false, children: []},
-      %{id: 10, name: "Ch 1-2-2", parent_id: 7, has_children: false, children: []},
-      %{id: 11, name: "Ch 3-1", parent_id: 7, has_children: false, children: []}
+      %{id: 1, name: "Ch 1", parent_id: nil, has_children: true},
+      %{id: 2, name: "Ch 2", parent_id: nil, has_children: false},
+      %{id: 3, name: "Ch 3", parent_id: nil, has_children: false},
+      %{id: 4, name: "Ch 4", parent_id: nil, has_children: false},
+      %{id: 5, name: "Ch 5", parent_id: nil, has_children: false},
+      %{id: 6, name: "Ch 1-1", parent_id: 1, has_children: false},
+      %{id: 7, name: "Ch 1-2", parent_id: 1, has_children: true},
+      %{id: 8, name: "Ch 1-3", parent_id: 1, has_children: false},
+      %{id: 9, name: "Ch 1-2-1", parent_id: 7, has_children: false},
+      %{id: 10, name: "Ch 1-2-2", parent_id: 7, has_children: false},
+      %{id: 11, name: "Ch 3-1", parent_id: 7, has_children: false}
     ]
   end
 end
