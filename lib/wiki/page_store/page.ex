93273b35d45eda0c9cc36422783fbac3b0e8acd7
ByeongUk Choi <cbw0916@gmail.com>
2022-02-22 23:56:03 +0900

    add page schema

---
+++ lib/wiki/page_store/page.ex
@@ -0,0 +1,28 @@
+defmodule Wiki.PageStore.Page do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @type id :: integer()
+  @type t :: %__MODULE__{
+          id: id,
+          title: String.t(),
+          content: String.t(),
+          inserted_at: DateTime.t(),
+          updated_at: DateTime.t()
+        }
+
+  schema "pages" do
+    field :title, :string
+    field :content, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(document, attrs) do
+    document
+    |> cast(attrs, [:title, :content])
+    |> validate_required([:title, :content])
+    |> validate_length(:title, max: 255)
+  end
+end
