6093e68a14456f48256bb3cbaa235d6b526ff281
ByeongUk Choi <cbw0916@gmail.com>
2022-03-28 21:56:29 +0900

    feat add page store postgres impl

--- lib/wiki/page_store/postgres_impl.ex
+++ lib/wiki/page_store/postgres_impl.ex
@@ -4,6 +4,8 @@ defmodule Wiki.PageStore.PostgreImpl do
   alias Wiki.Repo
   alias Wiki.PageStore.Page
 
+  import Ecto.Query
+
   @behaviour Wiki.PageStore
 
   @impl true
@@ -16,7 +18,29 @@ defmodule Wiki.PageStore.PostgreImpl do
   end
 
   @impl true
+  def fetch_all(parent_id: parent_id, page_num: page_num, per_page: per_page) do
+
+    from(p in Page, where: p.parent_id == ^parent_id, limit: ^per_page, offset: ^((page_num - 1) * per_page))
+    |> Repo.all()
+  end
+
+  @impl true
+  def fetch_all(page_num: page_num, per_page: per_page) do
+    from(p in Page, limit: ^per_page, offset: ^((page_num - 1) * per_page))
+    |> Repo.all()
+  end
+
+  @impl true
   def get_total_count() do
     Repo.aggregate(Page, :count)
+  end
+
+  @impl true
+  def create(title: title, content: content, parent_id: parent_id) do
+    Repo.insert(%Page{title: title, content: content, parent_id: parent_id})
+    |> case do
+      {:ok, page} -> {:ok, page}
+      {:error, _changeset} -> {:ok, :failed_create}
+    end
   end
 end
