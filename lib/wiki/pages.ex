f8e444ee2c521ec72cd80f9216386f7a7f236d12
ByeongUk Choi <cbw0916@gmail.com>
2022-08-27 03:01:35 +0900

    feat. add pages modules

--- lib/wiki/pages.ex
+++ lib/wiki/pages.ex
@@ -1,4 +1,30 @@
 defmodule Wiki.Pages do
+  @page_store Application.compile_env(:wiki, :page_store, Wiki.PageStore.PostgreImpl)
+
+  def get(id) do
+    @page_store.fetch_by_id(id)
+  end
+
+  def get_all(page_num, page_size) do
+    @page_store.fetch_all(page_num: page_num, per_page: page_size)
+  end
+
+  def get_all(parent_id, page_num, page_size) do
+    @page_store.fetch_all(parent_id: parent_id, page_num: page_num, per_page: page_size)
+  end
+
+  def create(title, content, parent_id) do
+    @page_store.create(title: title, content: content, parent_id: parent_id)
+  end
+
+  def delete(id) do
+    @page_store.delete_by_id(id)
+  end
+
+  def update(id, title, content) do
+    @page_store.update(id, title: title, content: content)
+  end
+
   def subscribe() do
     Phoenix.PubSub.subscribe(Wiki.PubSub, "pages")
   end
