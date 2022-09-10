6a107737942420a1fec093c61b99b17974876415
ByeongUk Choi <cbw0916@gmail.com>
2022-09-10 22:19:20 +0900

    feat. add page genserver update/2

--- lib/wiki/page.ex
+++ lib/wiki/page.ex
@@ -3,12 +3,18 @@ defmodule Wiki.Page do
 
   @page_store Application.compile_env(:wiki, :page_store, Wiki.PageStore.PostgreImpl)
 
-  def start_link(title: title, content: content, parent_id: parent_id) do
+  def new(title: title, content: content, parent_id: parent_id) do
     GenServer.start_link(__MODULE__, title: title, content: content, parent_id: parent_id)
   end
 
-  def start_link(id) do
+  def get(id) do
     GenServer.start_link(__MODULE__, id)
+  end
+
+  @spec update(pid(), %{optional(:title) => String.t(), optional(:content) => String.t()}) :: :ok
+  def update(pid, params) do
+    Map.take(params, [:title, :content])
+    GenServer.cast(pid, {:update, Map.take(params, [:title, :content])})
   end
 
   def init(title: title, content: content, parent_id: parent_id) do
@@ -17,5 +23,16 @@ defmodule Wiki.Page do
 
   def init(id) do
     @page_store.fetch_by_id(id)
+  end
+
+  def handle_cast({:update, params}, state) do
+    new_state =
+      params
+      |> Map.take([:title, :content])
+      |> Enum.reduce(state, fn {k, v}, acc_state ->
+        Map.put(acc_state, k, v)
+      end)
+
+    {:noreply, new_state}
   end
 end
