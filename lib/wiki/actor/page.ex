ef1c3bcd70a5148d4bf5411ae3bb64759034cb0a
ByeongUk Choi <cbw0916@gmail.com>
2022-10-09 23:53:34 +0900

    feat. fix page_actor

--- lib/wiki/actor/page.ex
+++ lib/wiki/actor/page.ex
@@ -1,9 +1,6 @@
 defmodule Wiki.Actor.Page do
   use GenServer
 
-  alias Wiki.Registry
-
-  @table :page
   @page_store Application.compile_env(:wiki, :page_store, Wiki.PageStore.MongoImpl)
 
   @type state :: %{
@@ -13,27 +10,7 @@ defmodule Wiki.Actor.Page do
           parent_id: integer() | nil
         }
 
-  defp get_pid(id) do
-    case GenServer.whereis({:global, {__MODULE__, id}}) do
-      nil -> start_link(id) |> elem(1)
-      pid -> pid
-    end
-  end
-
-  defp start_link(id) do
-    page =
-      case @page_store.fetch_by_id(id) do
-        {:ok, page} -> page
-        error -> error
-      end
-
-    case GenServer.start_link(__MODULE__, page, {:global, {__MODULE__, id}}) do
-      {:ok, pid} -> {:ok, pid}
-      {:error, {:already_started, pid}} -> {:ok, pid}
-      error -> error
-    end
-  end
-
+  @spec get(id :: integer()) :: state()
   def get(id) do
     case GenServer.whereis({:global, {__MODULE__, id}}) do
       nil -> start_link(id) |> elem(1)
@@ -45,22 +22,40 @@ defmodule Wiki.Actor.Page do
   @spec update(integer(), %{optional(:title) => String.t(), optional(:content) => String.t()}) ::
           :ok
   def update(id, params) do
-    pid = get_pid(id)
-    Map.take(params, [:title, :content])
-    GenServer.cast(pid, {:update, Map.take(params, [:title, :content])})
+    new_params = Map.take(params, [:title, :content])
+
+    id
+    |> get_pid()
+    |> GenServer.cast({:update, new_params})
   end
 
   def delete(id) do
-    pid = Registry.lookup(@table, id)
-    GenServer.cast(pid, :delete)
+    id
+    |> get_pid()
+    |> GenServer.cast(:delete)
   end
 
   def init(page) do
     {:ok, page}
   end
 
-  def handle_call(:state, _from, state) do
-    {:reply, state, state}
+  defp get_pid(id) do
+    case GenServer.whereis({:global, {__MODULE__, id}}) do
+      nil -> start_link(id) |> elem(1)
+      pid -> pid
+    end
+  end
+
+  defp start_link(id) do
+    with {:ok, page} <- @page_store.fetch_by_id(id) do
+      case GenServer.start_link(__MODULE__, page, name: {:global, {__MODULE__, id}}) do
+        {:ok, pid} -> {:ok, pid}
+        {:error, {:already_started, pid}} -> {:ok, pid}
+        error -> error
+      end
+    else
+      error -> error
+    end
   end
 
   def handle_cast({:update, params}, state) do
